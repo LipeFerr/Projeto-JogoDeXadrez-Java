7ff1d21961c2191b96170e8b654bf1d585d74f65
Felipe Alves <felipe.ferreira.alves6@gmail.com>
2023-04-13 09:47:12 -0300

    Check logic

--- src/application/Program.java
+++ src/application/Program.java
@@ -13,7 +13,6 @@ import chess.ChessPosition;
 public class Program {
 
 	public static void main(String[] args) {
-		
 		Scanner sc = new Scanner(System.in);
 		ChessMatch chessMatch = new ChessMatch();
 		List<ChessPiece> captured = new ArrayList<>();
@@ -29,8 +28,7 @@ public class Program {
 				boolean[][] possibleMoves = chessMatch.possibleMoves(source);
 				UI.clearScreen();
 				UI.printBoard(chessMatch.getPieces(), possibleMoves);
-				
-				
+								
 				System.out.println();
 				System.out.print("Target: ");
 				ChessPosition target = UI.readChessPosition(sc);
